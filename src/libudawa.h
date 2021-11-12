3f73f35dd9f34d3c3a9d6d41862e634230bea999
iwas108 <64204327+iwas108@users.noreply.github.com>
2021-11-13 00:41:39 +0800

    debug

--- src/libudawa.h
+++ src/libudawa.h
@@ -284,7 +284,7 @@ void iotInit()
         { "provisionResponse", processProvisionResponse },
         { "provisionResponse", processProvisionResponse }
       };
-      tb.callbackSubscribe(cb, 2);
+      Serial.println(tb.callbackSubscribe(cb, 2));
       if(tbProvision.sendProvisionRequest(config.name, config.provisionDeviceKey, config.provisionDeviceSecret))
       {
         config.provSent = true;
