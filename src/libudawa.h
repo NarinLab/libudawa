a142113bfc4cf3ab2801f4ee886f687bbfdcc28c
iwas108 <64204327+iwas108@users.noreply.github.com>
2022-04-15 23:01:31 +0800

    sram check

--- src/libudawa.h
+++ src/libudawa.h
@@ -258,7 +258,9 @@ void otaUpdateInit()
 void iotInit()
 {
   int freeHeap = heap_caps_get_free_size(MALLOC_CAP_8BIT);
-  if(freeHeap < 100000)
+  sprintf_P(logBuff, PSTR("Initializing IoT, available memory: %d"), freeHeap);
+  recordLog(4, PSTR(__FILE__), __LINE__, PSTR(__func__));
+  if(freeHeap < 32000)
   {
     sprintf_P(logBuff, PSTR("Unable to init IoT, insufficient memory: %d"), freeHeap);
     recordLog(4, PSTR(__FILE__), __LINE__, PSTR(__func__));
